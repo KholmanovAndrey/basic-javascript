963107f1eacdd6d87dbee66ec88e1222f8dd55e9
Kholmanov Andrey Home <ankholmanov@yandex.ru>
2019-03-13 18:34:59 +0700

    задание 3 доработка

--- js/lesson5_3.js
+++ js/lesson5_3.js
@@ -6,30 +6,58 @@
 // console.log(document.querySelector('input[name="name"'));
 
 document.querySelector('.btn').addEventListener('click', event => {
+    let valid = true;
     if (!validateName()) {
+        valid = false;
+    }
+    if (!validatePhone()) {
+        valid = false;
+    }
+    if (valid !== true) {
         event.preventDefault();
     }
 });
 
 function validateName() {
     const name = document.querySelector('input[name="name"]');
-    console.log(name.value);
     let value = name.value;
     if (value.trim().length < 1 || value.trim().length > 49){
-        console.log(value);
         renderMessage(name, `Имя - должно содержать как минимум 1 символ, не более 50 символов`);
         return false;
+    } else {
+        name.parentElement.querySelector('.invalid-feedback').style.display = 'none';
+        name.style.border = '1px solid #c0c0c0';
+    }
+    return true;
+}
+
+function validatePhone() {
+    const name = document.querySelector('input[name="phone"]');
+    let value = name.value;
+    if (value.trim().length !== 11){
+        renderMessage(name, `Телефон - должно содержать 11 символов`);
+        return false;
+    } else {
+        name.parentElement.querySelector('.invalid-feedback').style.display = 'none';
+        name.style.border = '1px solid #c0c0c0';
     }
     return true;
 }
 
 function renderMessage(element, messageText) {
     const parentElement = element.parentElement;
+    const invalid = parentElement.querySelector('.invalid-feedback');
+    if (invalid) {
+        element.style.border = '1px solid #c0c0c0';
+        invalid.remove();
+    }
     const messageDiv = document.createElement('div');
     messageDiv.innerHTML = messageText;
     messageDiv.classList.add('invalid-feedback');
+
     parentElement.appendChild(messageDiv);
     messageDiv.style.display = 'block';
+    element.style.border = '1px solid red';
 }
 
 // const form = {
