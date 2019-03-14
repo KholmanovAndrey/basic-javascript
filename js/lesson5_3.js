cf29c45792d3807357777b4b937c3c5145ba3ab3
Andrey Kholmanov Work <ankholmanov@yandex.ru>
2019-03-14 08:32:19 +0700

    урок 5 завершено 3 задание

--- js/lesson5_3.js
+++ js/lesson5_3.js
@@ -1,16 +1,49 @@
 "use strict";
 
-// console.log(document.querySelector('input[name="name"]').getAttribute('value'));
-// const value = document.querySelector('input[name="name"]');
-// value.setAttribute('value', 'dfdfdf');
-// console.log(document.querySelector('input[name="name"'));
+/*
+ Урок 5, задание 3:
+
+ Создать форму в html со следующими полями:
+ * Имя - текстовое поле
+ * Телефон - текстовое поле
+ * Пароль - поле типа password
+ * Повтор пароля - поле типа password
+ * Кнопка отправить форму
+
+ Необходимо реализовать валидацию этой формы по следующим правилам:
+
+ * Имя - должно содержать как минимум 1 символ, не более 50 символов.
+ * Телефон - должно содержать 11 цифр, не больше, не меньше.
+ * Пароль - минимум 5 символов, максимум 50.
+ * Повтор пароля - значение должно совпадать с полем пароль.
+ * Кнопка отправить форму - при нажатии на кнопку форма должна провериться, при прохождении проверки, форма
+ отправляется, если проверка не была пройдена, то:
+ - Каждое поле, которое не прошло проверку должно выделяться красным цветом.
+ - Под каждым полем, что не прошло проверку, должна писаться подсказка по какой причине проверка провалилась.
+
+ Можете пользоваться стилями бутстрапа, если лень самим писать.
+ Пользоваться аттрибутами HTML5 запрещено, необходимо проверки реализовать с помощью javascript.
+ */
 
 document.querySelector('.btn').addEventListener('click', event => {
+    const name = document.querySelector('input[name="name"]');
+    const phone = document.querySelector('input[name="phone"]');
+    const password = document.querySelector('input[name="password"]');
+    const password_repeat = document.querySelector('input[name="password_repeat"]');
     let valid = true;
-    if (!validateName()) {
+    if (!validateName(name)) {
         valid = false;
     }
-    if (!validatePhone()) {
+    if (!validatePhone(phone)) {
+        valid = false;
+    }
+    if (!validatePassword(password)) {
+        valid = false;
+    }
+    if (!validatePassword(password_repeat)) {
+        valid = false;
+    }
+    if (!validatePasswordMatch(password, password_repeat)) {
         valid = false;
     }
     if (valid !== true) {
@@ -18,39 +51,79 @@ document.querySelector('.btn').addEventListener('click', event => {
     }
 });
 
-function validateName() {
-    const name = document.querySelector('input[name="name"]');
-    let value = name.value;
-    if (value.trim().length < 1 || value.trim().length > 49){
-        renderMessage(name, `Имя - должно содержать как минимум 1 символ, не более 50 символов`);
+/**
+ * Валидация Имени на совпадение с условием ввода данных
+ * @param {object} element - объект проверки
+ * @returns {boolean} true если валидация прошла успешно
+ */
+function validateName(element) {
+    const value = element.value;
+    if (value.trim().length < 1 || value.trim().length > 50){
+        deleteMessage(element);
+        createMessage(element, `Имя - минимум 1 символ, максимум 50 символов, сейчас ${value.trim().length}.`);
         return false;
-    } else {
-        name.parentElement.querySelector('.invalid-feedback').style.display = 'none';
-        name.style.border = '1px solid #c0c0c0';
     }
+    deleteMessage(element);
     return true;
 }
 
-function validatePhone() {
-    const name = document.querySelector('input[name="phone"]');
-    let value = name.value;
+/**
+ * Валидация Телефона на совпадение с условием ввода данных
+ * @param {object} element - объект проверки
+ * @returns {boolean} true если валидация прошла успешно
+ */
+function validatePhone(element) {
+    const value = element.value;
     if (value.trim().length !== 11){
-        renderMessage(name, `Телефон - должно содержать 11 символов`);
+        deleteMessage(element);
+        createMessage(element, `Телефон - должно содержать 11 символов, сейчас ${value.trim().length}.`);
         return false;
-    } else {
-        name.parentElement.querySelector('.invalid-feedback').style.display = 'none';
-        name.style.border = '1px solid #c0c0c0';
     }
+    deleteMessage(element);
     return true;
 }
 
-function renderMessage(element, messageText) {
-    const parentElement = element.parentElement;
-    const invalid = parentElement.querySelector('.invalid-feedback');
-    if (invalid) {
-        element.style.border = '1px solid #c0c0c0';
-        invalid.remove();
+/**
+ * Валидация Пароля на совпадение с условием ввода данных
+ * @param {object} element - объект проверки
+ * @returns {boolean} true если валидация прошла успешно
+ */
+function validatePassword(element) {
+    const value = element.value;
+    if (value.trim().length < 5 || value.trim().length > 50){
+        deleteMessage(element);
+        createMessage(element, `Пароль - минимум 5 символов, максимум 50, сейчас ${value.trim().length}.`);
+        return false;
     }
+    deleteMessage(element);
+    return true;
+}
+
+/**
+ * Валидация Пароля на равенство двух паролей
+ * @param {object} element1  - 1 объект проверки
+ * @param {object} element2  - 2 объект проверки
+ * @returns {boolean}
+ */
+function validatePasswordMatch(element1, element2) {
+    const value1 = element1.value;
+    const value2 = element2.value;
+    if (value1 !== value2) {
+        deleteMessage(element2);
+        createMessage(element2, `Повтор пароля - значение должно совпадать с полем пароль.`);
+        return false;
+    }
+    deleteMessage(element2);
+    return true;
+}
+
+/**
+ * Создание и вывод сообщение о несоответсвий условию валидации
+ * @param {object} element - объект проверки
+ * @param {string} messageText - строка для вывода сообщения
+ */
+function createMessage(element, messageText) {
+    const parentElement = element.parentElement;
     const messageDiv = document.createElement('div');
     messageDiv.innerHTML = messageText;
     messageDiv.classList.add('invalid-feedback');
@@ -60,105 +133,15 @@ function renderMessage(element, messageText) {
     element.style.border = '1px solid red';
 }
 
-// const form = {
-//     container: null,
-//     name: null,
-//     phone: null,
-//     password: null,
-//     password_repeat: null,
-//
-//     /**
-//      * Инициализация объекта
-//      */
-//     init() {
-//         this.container = document.querySelector('.my-form');
-//         this.name = document.querySelector('input[name="name"]');
-//         this.phone = document.querySelector('input[name="phone"]');
-//         this.password = document.querySelector('input[name="password"]');
-//         this.password_repeat = document.querySelector('input[name="password_repeat"]');
-//     },
-//
-//     /**
-//      * Запуск объекта
-//      */
-//     run() {
-//         this.init();
-//         this.name.addEventListener('focus', this.validateName);
-//     },
-//
-//     validateName() {
-//         let messageText = `Минимальное значение 1`;
-//         console.log(event);
-//         form.renderMessage(event.target, messageText);
-//         // this.container.querySelector('.btn-primary').addEventListener('click', event => {
-//         //     console.log(this.validateValueForNull(this.name));
-//         //
-//         //     event.preventDefault();
-//         // });
-//     },
-//
-//     validateValueForNull(element) {
-//         return element.getAttribute('value');
-//     },
-//
-//     renderMessage(element, messageText) {
-//         const parentElement = element.parentElement;
-//         parentElement.removeChild('.invalid-feedback');
-//         const messageDiv = document.createElement('div');
-//         messageDiv.innerHTML = messageText;
-//         messageDiv.classList.add('invalid-feedback');
-//         parentElement.appendChild(messageDiv);
-//         messageDiv.style.display = 'block';
-//     },
-// };
-//
-// form.run();
-
-// const name = document.querySelector('input[name="name"]');
-// let value = name.setAttribute('value', 'dfhkdjsf');
-
-// const btn = document.querySelector('.btn-primary');
-// btn.addEventListener('click', event => {
-//     // if (!validateName() && !validatePhone()) {
-//     //     event.preventDefault();
-//     // }
-//     validateName();
-//     // validatePhone();
-//     event.preventDefault();
-// });
-//
-// function validateName() {
-//     const name = document.querySelector('input[name="name"]');
-//     console.log(name);
-//     let value = name.getAttribute('value');
-//     // if (value.trim().length < 1 || value.trim().length > 49){
-//     //     console.log(value);
-//     //     renderMessage(name, `Имя - должно содержать как минимум 1 символ, не более 50 символов`);
-//     //     return false;
-//     // }
-//     // return true;
-// }
-//
-// function validatePhone() {
-//     const name = document.querySelector('input[name="phone"]');
-//     let value = name.getAttribute('value');
-//     console.log(value.length);
-//     if (!value) {
-//         value = "";
-//     }
-//     console.log(value.length);
-//     if (value.length !== 11){
-//         renderMessage(name, `Телефон - должно содержать 11 цифр, не больше, не меньше`);
-//         return false;
-//     }
-//     return true;
-// }
-//
-// function renderMessage(element, messageText) {
-//     const parentElement = element.parentElement;
-//     const messageDiv = document.createElement('div');
-//     messageDiv.innerHTML = messageText;
-//     messageDiv.classList.add('invalid-feedback');
-//     parentElement.appendChild(messageDiv);
-//     messageDiv.style.display = 'block';
-// }
+/**
+ * Удаление сообщения
+ * @param {object} element - объект проверки
+ */
+function deleteMessage(element) {
+    const parentElement = element.parentElement;
+    const invalid = parentElement.querySelector('.invalid-feedback');
+    if (invalid) {
+        element.style.border = '1px solid #c0c0c0';
+        invalid.remove();
+    }
+}
